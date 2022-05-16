2b44286ce713231a2807bd68b69e0e39c2c14915
Carlos Reinaldo Garcia <kalliche16@gmail.com>
2022-05-16 05:06:43 -0500

    descripcion de scope global

---
+++ src/scope/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+var hello = 'Hello';
+let world = 'World';
+const helloWorld = 'Hello World';
+
+const anotherFumction = () => {
+    console.log(hello);
+    console.log(world);
+    console.log(helloWorld);
+}
+
+anotherFumction();
